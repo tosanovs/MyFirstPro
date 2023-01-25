c81a9bb2e11c53baa8ba6ac35b1fd31dcb677f0f
tosanovs <123589466+tosanovs@users.noreply.github.com>
2023-01-25 20:31:15 +0100

    Add files via upload
    My first commit

---
+++ MyFirstPro/src/tests/standalone/RegistrationFormTest.java
@@ -0,0 +1,27 @@
+package tests.standalone;
+
+public class RegistrationFormTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello word!");
+        sayHello( "Simon");
+
+}
+    ;public int getAge() {
+        int age;
+        return age;
+    }
+//TODO
+    public static void sayHello(String name) {
+    System.out.println("Hello" + name);
+}
+   //komentar
+
+
+    public RegistrationFormTest (int age) {
+        this.age = age;
+    }
+
+    public static void sayHallo (String name){
+     System.out.println("Hello" + name);}
+}
